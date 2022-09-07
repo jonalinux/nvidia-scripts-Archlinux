fbabe42648dc1564b137a0b603fc5ddf55c9e44d
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-09-07 14:00:09 +0100

    Update nvidia-install.sh

--- script nvidia/nvidia-install.sh
+++ script nvidia/nvidia-install.sh
@@ -3,17 +3,24 @@
 
 
 echo ""
-echo "Install Nvidia driver on Archlinux"
+echo "Install Nvidia driver and gpu chrome accelleration on Archlinux"
 echo ""
 
-sudo pacman -S nvidia
+sudo pacman -S nvidia libva-mesa-driver nvidia-settings libva-vdpau-driver libva-utils
+
 sudo mkdir -p /etc/pacman.d/hooks
 sudo cp -r nvidia.hook /etc/pacman.d/
 sudo cp -r blacklist-nvidia-nouveau.conf /etc/modprobe.d/
 sudo cp -r 20-nvidia.conf /etc/X11/xorg.conf.d/
 sudo cp -r mkinitcpio.conf /etc
+
 sudo mkinitcpio -P linux
-cp -r .xinitrc ~/
+
+echo "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder
+--enable-zero-copy
+--disable-features=UseChromeOSDirectVideoDecoder" > ~/.config/chrome-flags.conf
+
+
 
 
 echo ""
@@ -21,6 +28,7 @@ echo ""
 echo Nvidia drivers installed - Press enter to exit; 
 echo ""
 read
+
 
 
 
